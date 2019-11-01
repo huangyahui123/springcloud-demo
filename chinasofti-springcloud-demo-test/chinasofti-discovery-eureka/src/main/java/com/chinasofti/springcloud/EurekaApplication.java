fe7450961dd073f3cb6c9100c43196c43db06aa9
huangyahui123 <545235277@qq.com>
2019-11-02 01:24:10 +0800

    添加注解componentscan

--- chinasofti-springcloud-demo-test/chinasofti-discovery-eureka/src/main/java/com/chinasofti/springcloud/EurekaApplication.java
+++ chinasofti-springcloud-demo-test/chinasofti-discovery-eureka/src/main/java/com/chinasofti/springcloud/EurekaApplication.java
@@ -3,10 +3,12 @@ package com.chinasofti.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableEurekaServer
+//@ComponentScan
 public class EurekaApplication {
 	
   public static void main(String[] args) {
